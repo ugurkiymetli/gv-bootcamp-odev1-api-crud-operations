1a524026320bd3e892c8c7517e7dc1bb0b4b17ba
ugurkiymetli <34272634+ugurkiymetli@users.noreply.github.com>
2021-11-22 17:07:09 +0300

    DataAnnotations added to Contact class. AddContact method created (HttpPost).

--- Bootcamp-Project-Dragon-API/Controllers/ContactController.cs
+++ Bootcamp-Project-Dragon-API/Controllers/ContactController.cs
@@ -12,137 +12,137 @@ namespace Bootcamp_Project_Dragon_API.Controllers
         private static List<Contact> ContactList = new List<Contact>()
         {
              new Contact{
-                Id= 1,
+                ContactID= 1,
                 Name= "Silvanus",
                 Surname= "Carlino",
                 PhoneNumber= "6186536283",
-                ProfilePicture= "http://dummyimage.com/x.png/ff4444/ffffff",
+                ProfilePicture= "http://dummyimage.com/200x200.png/fff",
               },
               new Contact{
-                Id= 2,
+                ContactID= 2,
                 Name= "Gannie",
                 Surname= "Torrese",
                 PhoneNumber= "8429607324",
-                ProfilePicture= "http://dummyimage.com/x.png/cc0000/ffffff",
+                ProfilePicture= "http://dummyimage.com/200x200.png/fff",
               },
               new Contact{
-                Id= 3,
+                ContactID= 3,
                 Name= "Lissie",
                 Surname= "Wombwell",
                 PhoneNumber= "1078657306",
-                ProfilePicture= "http://dummyimage.com/x.png/5fa2dd/ffffff",
+                ProfilePicture= "http://dummyimage.com/200x200.png/fff",
               },
               new Contact{
-                Id= 4,
+                ContactID= 4,
                 Name= "Bobbee",
                 Surname= "Luppitt",
                 PhoneNumber= "3152299686",
-                ProfilePicture= "http://dummyimage.com/x.png/dddddd/000000",
+                ProfilePicture= "http://dummyimage.com/200x200.png/fff",
               },
               new Contact{
-                Id= 5,
+                ContactID= 5,
                 Name= "Linnet",
                 Surname= "MacCorkell",
                 PhoneNumber= "3921066098",
-                ProfilePicture= "http://dummyimage.com/x.png/5fa2dd/ffffff",
+                ProfilePicture= "http://dummyimage.com/200x200.png/fff",
               },
               new Contact{
-                Id= 6,
+                ContactID= 6,
                 Name= "Sol",
                 Surname= "Laetham",
                 PhoneNumber= "5132311122",
-                ProfilePicture= "http://dummyimage.com/x.png/cc0000/ffffff",
+                ProfilePicture= "http://dummyimage.com/200x200.png/fff",
               },
               new Contact{
-                Id= 7,
+                ContactID= 7,
                 Name= "Brannon",
                 Surname= "Tunbridge",
                 PhoneNumber= "3957222567",
-                ProfilePicture= "http://dummyimage.com/x.png/cc0000/ffffff",
+                ProfilePicture= "http://dummyimage.com/200x200.png/fff",
               },
               new Contact{
-                Id= 8,
+                ContactID= 8,
                 Name= "Ashia",
                 Surname= "Bracegirdle",
                 PhoneNumber= "9771322079",
-                ProfilePicture= "http://dummyimage.com/x.png/5fa2dd/ffffff",
+                ProfilePicture= "http://dummyimage.com/200x200.png/fff",
               },
               new Contact{
-                Id= 9,
+                ContactID= 9,
                 Name= "Eunice",
                 Surname= "Skevington",
                 PhoneNumber= "8167207899",
-                ProfilePicture= "http://dummyimage.com/x.png/ff4444/ffffff",
+                ProfilePicture= "http://dummyimage.com/200x200.png/fff",
               },
               new Contact{
-                Id= 10,
+                ContactID= 10,
                 Name= "Joli",
                 Surname= "Prozillo",
                 PhoneNumber= "4452608790",
-                ProfilePicture= "http://dummyimage.com/x.png/ff4444/ffffff",
+                ProfilePicture= "http://dummyimage.com/200x200.png/fff",
               },
               new Contact{
-                Id= 11,
+                ContactID= 11,
                 Name= "Donnell",
                 Surname= "Fabbri",
                 PhoneNumber= "6155401399",
-                ProfilePicture= "http://dummyimage.com/x.png/cc0000/ffffff",
+                ProfilePicture= "http://dummyimage.com/200x200.png/fff",
               },
               new Contact{
-                Id= 12,
+                ContactID= 12,
                 Name= "Shaylynn",
                 Surname= "Beston",
                 PhoneNumber= "9142672643",
-                ProfilePicture= "http://dummyimage.com/x.png/cc0000/ffffff",
+                ProfilePicture= "http://dummyimage.com/200x200.png/fff",
               },
               new Contact{
-                Id= 13,
+                ContactID= 13,
                 Name= "Diena",
                 Surname= "Gierth",
                 PhoneNumber= "1858750340",
-                ProfilePicture= "http://dummyimage.com/x.png/5fa2dd/ffffff",
+                ProfilePicture= "http://dummyimage.com/200x200.png/fff",
               },
               new Contact{
-                Id= 14,
+                ContactID= 14,
                 Name= "Armando",
                 Surname= "Reading",
                 PhoneNumber= "4697558840",
-                ProfilePicture= "http://dummyimage.com/x.png/cc0000/ffffff",
+                ProfilePicture= "http://dummyimage.com/200x200.png/fff",
               },
               new Contact{
-                Id= 15,
+                ContactID= 15,
                 Name= "Harp",
                 Surname= "Killbey",
                 PhoneNumber= "5913372515",
-                ProfilePicture= "http://dummyimage.com/x.png/cc0000/ffffff",
+                ProfilePicture= "http://dummyimage.com/200x200.png/fff",
               },
               new Contact{
-                Id= 16,
+                ContactID= 16,
                 Name= "Elle",
                 Surname= "Rowena",
                 PhoneNumber= "1522825136",
-                ProfilePicture= "http://dummyimage.com/x.png/ff4444/ffffff",
+                ProfilePicture= "http://dummyimage.com/200x200.png/fff",
               },
               new Contact{
-                Id= 17,
+                ContactID= 17,
                 Name= "Raf",
                 Surname= "Michie",
                 PhoneNumber= "7948358359",
-                ProfilePicture= "http://dummyimage.com/x.png/ff4444/ffffff",
+                ProfilePicture= "http://dummyimage.com/200x200.png/fff",
               },
               new Contact{
-                Id= 18,
+                ContactID= 18,
                 Name= "Smith",
                 Surname= "Boat",
                 PhoneNumber= "7824474755",
-                ProfilePicture= "http://dummyimage.com/x.png/dddddd/000000",
+                ProfilePicture= "http://dummyimage.com/200x200.png/fff",
               },
         };
 
         [HttpGet]
         public List<Contact> GetContacts()
         {
-            var contacts = ContactList.OrderBy(c => c.Id).ToList<Contact>();
+            var contacts = ContactList.OrderBy(c => c.ContactID).ToList<Contact>();
             return contacts;
         }
 
@@ -150,8 +150,22 @@ namespace Bootcamp_Project_Dragon_API.Controllers
 
         public Contact GetById( int id )
         {
-            var contact = ContactList.Where(c => c.Id == id).SingleOrDefault();
+            var contact = ContactList.Where(c => c.ContactID == id).SingleOrDefault();
             return contact;
         }
+
+        [HttpPost]
+        public IActionResult AddContact( [FromBody] Contact newContact )
+        {
+            var contact = ContactList.SingleOrDefault(c => c.PhoneNumber == newContact.PhoneNumber || c.ContactID == newContact.ContactID);
+            if ( contact is not null )
+            {
+                return BadRequest();
+            }
+
+            ContactList.Add(newContact);
+            return Ok();
+        }
+
     }
 }
