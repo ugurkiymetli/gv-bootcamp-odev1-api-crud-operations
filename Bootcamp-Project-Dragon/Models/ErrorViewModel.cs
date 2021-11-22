1832223ca3d1a56daa61d0d4b276f59c6d737f7a
ugurkiymetli <34272634+ugurkiymetli@users.noreply.github.com>
2021-11-22 14:52:14 +0300

    mvc project created

---
+++ Bootcamp-Project-Dragon/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Bootcamp_Project_Dragon.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
